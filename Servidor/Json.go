cee9f0c01d1832596c4f85b8236c9f4777ebc30c
Coca <cocacore7@gmail.com>
2021-02-16 00:52:04 -0600

    Quinto Commit

--- Servidor/Json.go
+++ Servidor/Json.go
@@ -1,10 +1,10 @@
 package Servidor
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
-var matriz [][]string
 var vec []lista
 
 type Datos struct{
@@ -28,20 +28,18 @@ type Tiendas struct {
 	Calificacion int `json:"Calificacion"`
 }
 
+type varios struct {
+	Tiendas []Tiendas `json:"Tiendas"`
+}
+
+type unico struct {
+	Departamento string `json:"Departamento"`
+	Tienda string `json:"Nombre"`
+	Calificacion int `json:"Calificacion"`
+}
+
 func Crear(data Datos){
 	vec = make([]lista, 0, len(data.Datos[0].Departamentos)*len(data.Datos)*5)
-	matriz = make([][]string, 0, len(data.Datos))
-
-	//Creando Matriz
-	for i := 0; i < len(data.Datos); i++ {
-		var aux = make([]string, 0, len(data.Datos[0].Departamentos))
-		for j:=0; j<len(data.Datos[i].Departamentos);j++{
-			aux = append(aux, data.Datos[i].Indice + "," + data.Datos[i].Departamentos[j].Departamentos)
-		}
-		matriz = append(matriz, aux)
-	}
-
-	fmt.Println(matriz)
 
 	//Creando Vector E Insertando Listas con Tiendas "ROW-MAJOR"
 	for i := 0; i < len(data.Datos); i++ {
@@ -85,70 +83,42 @@ func grafico1(){
 	reportes(vec,0,0,1)
 }
 
-/*func Leer2(){
-	archivo, err := os.Open("C:\\Users\\usuario\\OneDrive\\Escritorio\\primero.json")
-	if err != nil{
-		fmt.Println(err)
-	}
-	defer archivo.Close()
-
-	valorB, _ := ioutil.ReadAll(archivo)
-	var data Datos
-
-	err = json.Unmarshal(valorB, &data)
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	for i := 0; i < len(data.Datos); i++ {
-		fmt.Println("Indice: " + data.Datos[i].Indice)
-		for j:=0; j<len(data.Datos[i].Departamentos);j++{
-			fmt.Println("Departamento: " + data.Datos[i].Departamentos[j].Departamentos)
-			for x:=0;x<len(data.Datos[i].Departamentos[j].Tiendas);x++{
-				fmt.Println("Tienda: " + data.Datos[i].Departamentos[j].Tiendas[x].Tiendas)
-				fmt.Println("Contacto: " + data.Datos[i].Departamentos[j].Tiendas[x].Descripcion)
-				fmt.Println("Descripcion: " + data.Datos[i].Departamentos[j].Tiendas[x].Contacto)
-				fmt.Println("Calificacion: " + strconv.Itoa(data.Datos[i].Departamentos[j].Tiendas[x].Calificacion))
+func posicionl(i int) []byte{
+	if vec != nil{
+		if vec[i-1].Vacio(){
+			crear_json, _ := json.Marshal("Lista Vacia")
+			return crear_json
+		}else{
+			var v varios
+			a := vec[i-1].primero
+			for a != nil{
+				t := Tiendas{Tiendas:a.tienda.nombre,Descripcion:a.tienda.descripcion,Contacto:a.tienda.contacto,Calificacion:a.tienda.calif}
+				v.Tiendas = append(v.Tiendas, t)
+				a = a.sig
 			}
+			crear_json, _ := json.Marshal(v)
+			return crear_json
 		}
+	}else{
+		crear_json, _ := json.Marshal("No Hay Tiendas Cargadas")
+		return crear_json
 	}
+}
 
-	//Creando Vector E Insertando Listas con Tiendas "ROW-MAJOR"
+func posiciont(t unico) Tiendas{
+	var ti Tiendas
 	for i := 0; i < len(data.Datos); i++ {
 		for j:=0; j<len(data.Datos[i].Departamentos);j++{
-			l1 := newLista()
-			l2 := newLista()
-			l3 := newLista()
-			l4 := newLista()
-			l5 := newLista()
-			for x:=0;x<len(data.Datos[i].Departamentos[j].Tiendas);x++{
-				t := newTienda(data.Datos[i].Departamentos[j].Tiendas[x].Tiendas, data.Datos[i].Departamentos[j].Tiendas[x].Descripcion, data.Datos[i].Departamentos[j].Tiendas[x].Contacto, data.Datos[i].Departamentos[j].Tiendas[x].Calificacion)
-				if data.Datos[i].Departamentos[j].Tiendas[x].Calificacion == 1 {
-					insertar(t,l1)
-				} else if data.Datos[i].Departamentos[j].Tiendas[x].Calificacion == 2{
-					insertar(t,l2)
-				} else if data.Datos[i].Departamentos[j].Tiendas[x].Calificacion == 3{
-					insertar(t,l3)
-				} else if data.Datos[i].Departamentos[j].Tiendas[x].Calificacion == 4{
-					insertar(t,l4)
-				} else if data.Datos[i].Departamentos[j].Tiendas[x].Calificacion == 5{
-					insertar(t,l5)
+			if data.Datos[i].Departamentos[j].Departamentos == t.Departamento{
+				for z:=0; z<len(data.Datos[i].Departamentos[j].Tiendas);z++{
+					if data.Datos[i].Departamentos[j].Tiendas[z].Tiendas == t.Tienda{
+						if data.Datos[i].Departamentos[j].Tiendas[z].Calificacion == t.Calificacion{
+							ti = data.Datos[i].Departamentos[j].Tiendas[z]
+						}
+					}
 				}
 			}
-			vec = append(vec, *l1)
-			vec = append(vec, *l2)
-			vec = append(vec, *l3)
-			vec = append(vec, *l4)
-			vec = append(vec, *l5)
 		}
 	}
-
-	fmt.Println(vec)
-
-	//Ordenar Valores en Listas
-	for i:=0; i<len(vec);i++{
-		vec[i] = vec[i].ordenar()
-	}
-
-	reportes(vec,0,0,1)
-}*/
+	return ti
+}
--- Servidor/Servidor.go
+++ Servidor/Servidor.go
@@ -7,10 +7,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
+var data Datos
+
 func cargar(w http.ResponseWriter, r *http.Request){
-	var data Datos
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil{
 		_, _ = fmt.Fprintf(w, "Error al insertar")
@@ -24,14 +26,37 @@ func cargar(w http.ResponseWriter, r *http.Request){
 
 func getArreglo(w http.ResponseWriter, r *http.Request){
 	grafico1()
+	w.Header().Set("Content-Type","applicattion/json")
+	w.WriteHeader(http.StatusCreated)
 }
 
 func tiendaE(w http.ResponseWriter, r *http.Request){
+	var t unico
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil{
+		_, _ = fmt.Fprintf(w, "Error al insertar")
+	}
+	w.Header().Set("Content-Type","applicattion/json")
+	w.WriteHeader(http.StatusFound)
+	_ = json.Unmarshal(body, &t)
+	a := posiciont(t)
+	if a.Calificacion != 0{
+		_ = json.NewEncoder(w).Encode(a)
+	}else{
+		_ = json.NewEncoder(w).Encode("No Se Encontro Ninguna Tienda Que Cumpla Con Los Parametros")
+	}
 
 }
 
 func tiendaN(w http.ResponseWriter, r *http.Request){
-
+	var v varios
+	vars:=mux.Vars(r)
+	b,_:=strconv.Atoi(vars["numero"])
+	a := posicionl(b)
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusFound)
+	_ = json.Unmarshal(a, &v)
+	_ = json.NewEncoder(w).Encode(v)
 }
 
 func elim(w http.ResponseWriter, r *http.Request){
@@ -41,7 +66,7 @@ func elim(w http.ResponseWriter, r *http.Request){
 func Iniciar(){
 	router := mux.NewRouter()
 	router.HandleFunc("/getArreglo", getArreglo).Methods("GET")
-	router.HandleFunc("/id/:numero", tiendaN).Methods("GET")
+	router.HandleFunc("/id/{numero}", tiendaN).Methods("GET")
 	router.HandleFunc("/cargartienda", cargar).Methods("POST")
 	router.HandleFunc("/TiendaEspecifica", tiendaE).Methods("POST")
 	router.HandleFunc("/Eliminar", elim).Methods("POST")
